c3356748e85146dd409faf47015597de8f1dfef4
Andynlx01 <andressaamaro495@gmail.com>
2024-09-17 09:02:35 -0300

    paginas PHP
    primeira atualização

---
+++ atividade - aula06/principal.php
@@ -0,0 +1,14 @@
+<!-- página principal (index)-->
+<?php
+include_once ('topo.php');
+include_once ('menu.php');
+?>
+
+<main>
+    <h2>Bem-vindo à Página Inicial</h2>
+    <p>Esta é a página principal do nosso site. Aqui você encontrará informações sobre nossos serviços e como podemos ajudar você!</p>
+</main>
+
+<?php
+include_once 'rodape.php';
+?>
