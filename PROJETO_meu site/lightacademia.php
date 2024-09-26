b7f6d4c2b2d6afe30eb2d6009ea1b49b3204b8a6
Andynlx01 <andressaamaro495@gmail.com>
2024-09-26 08:57:45 -0300

    projeto
    atualização

--- PROJETO_meu site/lightacademia.php
+++ PROJETO_meu site/lightacademia.php
@@ -1,12 +1,12 @@
 
 <main>
-    <h2>Light</h2>
+    <h2>Light Academia</h2>
     <p>Veja abaixo algumas estéticas de Light Academia e divirta-se!</p>
 
     <div class="galeria">
         <!-- Primeira Imagem -->
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/564x/30/04/bb/3004bb4d32fb69db134a0a0efaabfa7f.jpg" alt="foto 1">
+            <img src="https://i.pinimg.com/474x/bf/8f/14/bf8f140af2c19ac9232c6c01f655ca9c.jpg" alt="foto 1">
             <a href="imagens/desenvolvimento_web1.jpg" download="light_academia_wallpaper01.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
@@ -14,7 +14,7 @@
 
         <!-- Segunda Imagem -->
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/41/d2/be/41d2be17c0a5c25601ec1863a3b943d0.jpg" alt="foto 2">
+            <img src="https://i.pinimg.com/236x/2c/3f/22/2c3f22478dacf5d1566966c7eca5ef9b.jpg" alt="foto 2">
             <a href="imagens/desenvolvimento_web2.jpg" download="light_academia_wallpaper02.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
@@ -22,113 +22,113 @@
 
         <!-- Terceira Imagem -->
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/5b/5b/de/5b5bded5f896f5810fbcb2c5ba360736.jpg" alt="foto 3">
+            <img src="https://i.pinimg.com/236x/4a/50/b6/4a50b67185373e1fe83a41fa406c01ad.jpg" alt="foto 3">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_wallpaper03.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/19/6e/06/196e062c464fb32ff3fda59c6fddf4c8.jpg" alt="foto 4">
+            <img src="https://i.pinimg.com/236x/01/76/d2/0176d2b7ce86676436e3fd57f5d17bc1.jpg" alt="foto 4">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_wallpaper04.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/3b/07/17/3b07175d685ff3d095a6cf6239977d5b.jpg" alt="foto 5">
+            <img src="https://i.pinimg.com/564x/58/a4/c5/58a4c5b3213525ad323294fb7c704942.jpg" alt="foto 5">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_wallpaper05.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/9f/f1/f5/9ff1f5d5863bf46765917417f5512c57.jpg" alt="foto 6">
+            <img src="https://i.pinimg.com/236x/f0/84/39/f084397b451a66755057542ed94fdb70.jpg" alt="foto 6">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_wallpaper06.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/13/ce/66/13ce66ef08d1c36d2b3c0e8bd08e5179.jpg" alt="foto 7">
+            <img src="https://i.pinimg.com/564x/f8/64/31/f86431e329d776c95c10080725bbe37a.jpg" alt="foto 7">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon01.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/83/5a/21/835a219b3233ec840dedeadc6c9dce96.jpg" alt="foto 8">
+            <img src="https://i.pinimg.com/236x/98/78/f0/9878f0457ba629dfb0ac723f7542fa53.jpg" alt="foto 8">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon02.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/d1/f2/59/d1f25930229f23c3f09cc86846d2d1b0.jpg" alt="foto 9">
+            <img src="https://i.pinimg.com/236x/5f/64/06/5f640687f49aee0a0ff8407fb6fe6fd9.jpg" alt="foto 9">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon03.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/55/77/81/5577812867cca455eeca064b391e6c7c.jpg" alt="foto 10">
+            <img src="https://i.pinimg.com/236x/fb/be/71/fbbe714b0a80074a002a1509dc4d2d06.jpg" alt="foto 10">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon04.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/cc/77/db/cc77dbd6ee12ef8f23b000f0e2fb267a.jpg" alt="foto 11">
+            <img src="https://i.pinimg.com/236x/30/d6/29/30d629af09938d2e0b3fd43375e0c0ca.jpg" alt="foto 11">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon05.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/736x/2e/b4/15/2eb4156d244d6eed706b6b9b2c8eac05.jpg" alt="foto 12">
+            <img src="https://i.pinimg.com/236x/54/37/af/5437afdcd7c517ca50a64f022cb6f3ed.jpg" alt="foto 12">
             <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon06.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 13">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/5c/8c/e7/5c8ce7942de6698a66911004873e100a.jpg" alt="foto 13">
+            <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon07.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 14">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/d1/47/c1/d147c13bef4148a7843c781090af07f3.jpg" alt="foto 14">
+            <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon08.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 15">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/73/31/c5/7331c56b610d8273207ef8f686abc198.jpg" alt="foto 15">
+            <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon09.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 16">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/18/fe/df/18fedf0d3de421ff7c3f4fa9b60301ba.jpg" alt="foto 16">
+            <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon10.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 17">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/33/33/68/333368e38d841a9c2932f24e3603d759.jpg" alt="foto 17">
+            <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon11.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 18">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/3c/9d/e4/3c9de43fb076940760f226f58eb1c8ed.jpg" alt="foto 18">
+            <a href="imagens/desenvolvimento_web3.jpg" download="light_academia_icon12.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
@@ -136,5 +136,5 @@
         <!-- Adicionar mais imagens conforme necessário -->
     </div>
 
-    <a href="servicos.php">Voltar para a lista de serviços</a>
+    ><a href="?pg=servicos">Voltar</a>
 </main>
--- PROJETO_meu site/wonderland.php
+++ PROJETO_meu site/wonderland.php
@@ -6,7 +6,7 @@
     <div class="galeria">
         <!-- Primeira Imagem -->
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/564x/30/04/bb/3004bb4d32fb69db134a0a0efaabfa7f.jpg" alt="foto 1">
+            <img src="https://i.pinimg.com/236x/3c/3f/28/3c3f28322efd77c576eef46f2ed9572e.jpg" alt="foto 1">
             <a href="imagens/desenvolvimento_web1.jpg" download="alice_wallpaper01.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
@@ -14,7 +14,7 @@
 
         <!-- Segunda Imagem -->
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/41/d2/be/41d2be17c0a5c25601ec1863a3b943d0.jpg" alt="foto 2">
+            <img src="https://i.pinimg.com/236x/f7/23/c5/f723c5adf97471c16451cbf879a28d2a.jpg" alt="foto 2">
             <a href="imagens/desenvolvimento_web2.jpg" download="alice_wallpaper02.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
@@ -22,113 +22,113 @@
 
         <!-- Terceira Imagem -->
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/5b/5b/de/5b5bded5f896f5810fbcb2c5ba360736.jpg" alt="foto 3">
+            <img src="https://i.pinimg.com/236x/6e/72/15/6e7215eb775a804d8159d7a94941a8de.jpg" alt="foto 3">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_wallpaper03.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/19/6e/06/196e062c464fb32ff3fda59c6fddf4c8.jpg" alt="foto 4">
+            <img src="https://i.pinimg.com/236x/35/5f/9b/355f9bfdfdf7337b59f518ce5874e6e6.jpg" alt="foto 4">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_wallpaper04.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/3b/07/17/3b07175d685ff3d095a6cf6239977d5b.jpg" alt="foto 5">
+            <img src="https://i.pinimg.com/236x/0f/49/34/0f49346fe7b9710fa8bf0705357c224e.jpg" alt="foto 5">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_wallpaper05.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/9f/f1/f5/9ff1f5d5863bf46765917417f5512c57.jpg" alt="foto 6">
+            <img src="https://i.pinimg.com/236x/a8/62/f3/a862f3e996d801cc18bb9ee7ce31bd1f.jpg" alt="foto 6">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_wallpaper06.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/13/ce/66/13ce66ef08d1c36d2b3c0e8bd08e5179.jpg" alt="foto 7">
+            <img src="https://i.pinimg.com/236x/c8/45/6a/c8456aad3a7467d115b2f7aa33f9a238.jpg" alt="foto 7">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon01.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/474x/83/5a/21/835a219b3233ec840dedeadc6c9dce96.jpg" alt="foto 8">
+            <img src="https://i.pinimg.com/236x/e3/4a/6e/e34a6ef6ab8b888896f44543d1aee8f9.jpg" alt="foto 8">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon02.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/d1/f2/59/d1f25930229f23c3f09cc86846d2d1b0.jpg" alt="foto 9">
+            <img src="https://i.pinimg.com/236x/9c/a7/e7/9ca7e7208b5170cb59fc9e04c6e44a27.jpg" alt="foto 9">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon03.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/55/77/81/5577812867cca455eeca064b391e6c7c.jpg" alt="foto 10">
+            <img src="https://i.pinimg.com/474x/c0/0e/7b/c00e7b717cb57027b0f875a1b06efef6.jpg" alt="foto 10">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon04.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/236x/cc/77/db/cc77dbd6ee12ef8f23b000f0e2fb267a.jpg" alt="foto 11">
+            <img src="https://i.pinimg.com/236x/4c/e1/f0/4ce1f0b4b1b4db96de22837fc41cd159.jpg" alt="foto 11">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon05.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="https://i.pinimg.com/736x/2e/b4/15/2eb4156d244d6eed706b6b9b2c8eac05.jpg" alt="foto 12">
+            <img src="https://i.pinimg.com/236x/4d/71/e7/4d71e72a5f9086afdacb1c285d17c9c7.jpg" alt="foto 12">
             <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon06.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 13">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/dd/c0/64/ddc064f623c01e9fe5fde0e421e5cf6c.jpg" alt="foto 13">
+            <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon07.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 14">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/da/5e/4c/da5e4c90bc02c69060a41b60648a2918.jpg" alt="foto 14">
+            <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon08.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 15">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/06/e5/9e/06e59e643e0cbf5a77abe36d3d5c4e32.jpg" alt="foto 15">
+            <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon09.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 16">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/18/e5/58/18e55842b75c27bc2416e7525adc1ec5.jpg" alt="foto 16">
+            <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon10.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 17">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/236x/a1/1a/52/a11a52ea5f29a3f4b2b22d2bdce5a981.jpg" alt="foto 17">
+            <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon11.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
 
         <div class="imagem-item">
-            <img src="imagens/desenvolvimento_web3_thumb.jpg" alt="foto 18">
-            <a href="imagens/desenvolvimento_web3.jpg" download="desenvolvimento_web_projeto3.jpg">
+            <img src="https://i.pinimg.com/564x/77/04/ad/7704ad2b145e54b91f6fba15fe4ec4b2.jpg" alt="foto 18">
+            <a href="imagens/desenvolvimento_web3.jpg" download="alice_icon12.jpg">
                 <button class="botao-baixar">Baixar Imagem</button>
             </a>
         </div>
