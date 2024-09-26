b2a65e948ad7777737baa0061e72996372ee0259
Andynlx01 <andressaamaro495@gmail.com>
2024-09-26 06:49:27 -0300

    .
    .

--- PROJETO_meu site/principal.php
+++ PROJETO_meu site/principal.php
@@ -1,4 +1,14 @@
+<!-- principal.php -->
+<?php
+include_once 'topo.php';
+include_once 'menu.php';
+?>
+
 <main>
-    <h2>Bem-vindo à Página Inicial</h2>
-    <p>Esta é a página principal do nosso site. Aqui você encontrará informações sobre nossos serviços e como podemos ajudar você!</p>
+    <h2>Bem-vindo à Página Inicial💕</h2>
+    <p>Esta é a página principal do nosso site. Aqui você encontrará informações sobre nossos serviços e como podemos ajudar você.</p>
 </main>
+
+<?php
+include_once 'rodape.php';
+?>
